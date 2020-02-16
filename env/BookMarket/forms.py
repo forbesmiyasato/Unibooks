e7f2c02b7e9ee3ababb70207b36291e46421253b
forbesmiyasato <miya5288@pacificu.edu>
2020-02-15 22:58:10 -0800

    Initial commit, some functionalities working already

---
+++ env/BookMarket/forms.py
@@ -0,0 +1,59 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, MultipleFileField, SelectField, DecimalField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, InputRequired
+from BookMarket.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                'That email is taken. Please Choose a different one')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    'That email is taken. Please Choose a different one')
+
+
+class PostForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    price = DecimalField('Price', validators=[InputRequired()])
+    images = MultipleFileField('Upload item images', validators=[FileAllowed(['jpg', 'png'])])
+    item_department = SelectField('Department', coerce=int, validators=[InputRequired()])
+    item_class = SelectField('Class', coerce=int, validators=[InputRequired()])
+    submit = SubmitField('Post')
---
+++ env/BookMarket/models.py
@@ -0,0 +1,57 @@
+from BookMarket import db, login_manager
+from datetime import datetime
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    items = db.relationship('Item', backref='owner', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class ItemClass(db.Model):
+    __tablename__ = 'itemclass'
+    id = db.Column(db.Integer, primary_key=True)
+    class_name = db.Column(db.String(100), nullable=False)
+    department_id = db.Column(db.Integer, db.ForeignKey('itemdepartment.id'), nullable=False)
+
+
+class ItemDepartment(db.Model):
+    __tablename__ = 'itemdepartment'
+    id = db.Column(db.Integer, primary_key=True)
+    department_name = db.Column(db.String(100), nullable=False)
+    classes = db.relationship('ItemClass', backref='parent', lazy=True)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.DECIMAL(10, 2), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey('itemclass.id'), nullable=False)
+    department_id = db.Column(db.Integer, db.ForeignKey('itemdepartment.id'), nullable=False)
+    images = db.relationship('ItemImage', backref='owner', lazy=True)
+
+    def __repr__(self):
+        return f"Post('{self.name}', '{self.date_posted}')"
+
+
+class ItemImage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image_file = db.Column(db.String(20), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
