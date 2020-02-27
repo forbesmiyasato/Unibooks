3708846d7f94c09c134648ca71b85ec2ea49a1c3
forbesmiyasato <miya5288@pacificu.edu>
2020-02-27 00:30:21 -0800

    Posts to s3 file system

--- BookMarket/__init__.py
+++ BookMarket/__init__.py
@@ -1,4 +1,5 @@
 import os
+import boto3
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -6,6 +7,15 @@ from flask_login import LoginManager
 from dotenv import load_dotenv
 # from sassutils.wsgi import SassMiddleware
 
+# S3 configurations
+S3_BUCKET = os.getenv('S3_STORAGE_BUCKET')
+S3_KEY = os.getenv('AWS_ACCESS_KEY_ID')
+S3_SECRET = os.getenv('AWS_SECRET_ACCESS_KEY')
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=S3_KEY,
+    aws_secret_access_key=S3_SECRET
+)
 app = Flask(__name__)
 
 if __name__ == '__main__':
--- BookMarket/routes.py
+++ BookMarket/routes.py
@@ -1,11 +1,11 @@
 import os
 import secrets
-import logging
+import boto3
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort, jsonify
 from BookMarket.models import User, Item, ItemClass, ItemDepartment, ItemImage, SaveForLater
 from BookMarket.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
-from BookMarket import app, db, bcrypt
+from BookMarket import app, db, bcrypt, S3_BUCKET, s3
 from flask_login import login_user, current_user, logout_user, login_required
 from werkzeug.utils import secure_filename
 
@@ -259,21 +259,23 @@ def delete_saved():
 def save_picture(form_images, item_id):
     thumbnail = None
     for index, images in enumerate(form_images):
-
         if images:
             random_hex = secrets.token_hex(8)
             _, f_ext = os.path.splitext(images.filename)
             picture_fn = random_hex + f_ext
             if (index == 0):
                 thumbnail = picture_fn
-            picture_path = os.path.join(
-                app.root_path, 'static/item_pics', picture_fn)
-            output_size = (183, 195)
-            # output_resolution = (1000, 1000)
-            resizedImage = Image.open(images)
-            # resizedImage.thumbnail(output_resolution)
-            resizedImage = resizedImage.resize(output_size, Image.ANTIALIAS)
-            resizedImage.save(picture_path)
+            # picture_path = os.path.join(
+            #     app.root_path, 'static/item_pics', picture_fn)
+            # output_size = (183, 195)
+            # # output_resolution = (1000, 1000)
+            # resizedImage = Image.open(images)
+            # # resizedImage.thumbnail(output_resolution)
+            # resizedImage = resizedImage.resize(output_size, Image.ANTIALIAS)
+            # resizedImage.save(picture_path)
+            s3_resource = boto3.resource('s3')
+            my_bucket = s3_resource.Bucket(S3_BUCKET)
+            my_bucket.Object(picture_fn).put(Body=images)
             newImage = ItemImage(item_id=item_id, image_file=picture_fn)
             db.session.add(newImage)
             db.session.commit()
