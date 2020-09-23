53a94c67fa13336fc9d870883c5d4bfcd2e2f5f6
forbesmiyasato <miya5288@pacificu.edu>
2020-09-22 21:18:58 -0700

    fixed date issue and upgraded jquery

--- BookMarket/models.py
+++ BookMarket/models.py
@@ -53,7 +53,7 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False,
-                            default=datetime.utcnow())
+                            default=datetime.utcnow)
     description = db.Column(db.Text, nullable=True)
     price = db.Column(db.DECIMAL(10, 2), nullable=False)
     thumbnail = db.Column(db.String, nullable=False,
--- BookMarket/static/js/main.js
+++ BookMarket/static/js/main.js
@@ -23,12 +23,12 @@ $(document).ready(function () {
     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
   });
 
-  $('.img-pop-up').magnificPopup({
-    type: 'image',
-    gallery: {
-      enabled: true
-    }
-  });
+  // $('.img-pop-up').magnificPopup({
+  //   type: 'image',
+  //   gallery: {
+  //     enabled: true
+  //   }
+  // });
 
   // Search Toggle
   $("#search_input_box").hide();
@@ -145,42 +145,42 @@ $(document).ready(function () {
 
   // -------   Mail Send ajax
 
-  $(document).ready(function () {
-    var form = $('#booking'); // contact form
-    var submit = $('.submit-btn'); // submit button
-    var alert = $('.alert-msg'); // alert div for show alert message
+  // $(document).ready(function () {
+  //   var form = $('#booking'); // contact form
+  //   var submit = $('.submit-btn'); // submit button
+  //   var alert = $('.alert-msg'); // alert div for show alert message
 
-    // form submit event
-    form.on('submit', function (e) {
-      e.preventDefault(); // prevent default form submit
+  //   // form submit event
+  //   form.on('submit', function (e) {
+  //     e.preventDefault(); // prevent default form submit
 
-      $.ajax({
-        url: 'booking.php', // form action url
-        type: 'POST', // form submit method get/post
-        dataType: 'html', // request type html/json/xml
-        data: form.serialize(), // serialize form data
-        beforeSend: function () {
-          alert.fadeOut();
-          submit.html('Sending....'); // change submit button text
-        },
-        success: function (data) {
-          alert.html(data).fadeIn(); // fade in response data
-          form.trigger('reset'); // reset form
-          submit.attr("style", "display: none !important");; // reset submit button text
-        },
-        error: function (e) {
-          console.log(e)
-        }
-      });
-    });
-  });
-
-
+  //     $.ajax({
+  //       url: 'booking.php', // form action url
+  //       type: 'POST', // form submit method get/post
+  //       dataType: 'html', // request type html/json/xml
+  //       data: form.serialize(), // serialize form data
+  //       beforeSend: function () {
+  //         alert.fadeOut();
+  //         submit.html('Sending....'); // change submit button text
+  //       },
+  //       success: function (data) {
+  //         alert.html(data).fadeIn(); // fade in response data
+  //         form.trigger('reset'); // reset form
+  //         submit.attr("style", "display: none !important");; // reset submit button text
+  //       },
+  //       error: function (e) {
+  //         console.log(e)
+  //       }
+  //     });
+  //   });
+  // });
 
 
-  $(document).ready(function () {
-    $('#mc_embed_signup').find('form').ajaxChimp();
-  });
+
+
+  // $(document).ready(function () {
+  //   $('#mc_embed_signup').find('form').ajaxChimp();
+  // });
 
 
 
